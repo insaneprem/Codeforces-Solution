1af2d17399bbeaafe204bf9e884b647047229cea
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:54:02 +0530

    Add George and Accommodation [A] from Codeforces

---
+++ 467/A - George and Accommodation/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    int ans = 0;
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+ 
+        if (a + 2 <= b)
+            ans++;
+    }
+ 
+    cout << ans;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
