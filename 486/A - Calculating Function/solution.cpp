1717e5ebecce93f5b506d2bab013ccdb6dda490a
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:52:41 +0530

    Add Calculating Function [A] from Codeforces

---
+++ 486/A - Calculating Function/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    if (n % 2 == 0)
+        cout << n / 2;
+    else
+        cout << "-" << (n + 1) / 2;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
