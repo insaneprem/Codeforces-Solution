e8bea60f64334a701c7678b376a5d57c0bdb36a2
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:50:15 +0530

    Add Helpful Maths [A] from Codeforces

---
+++ 339/A - Helpful Maths/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    string str;
+    cin >> str;
+ 
+    sort(str.begin(), str.end());
+    string ans = "";
+    for (auto i : str)
+    {
+        if (i != '+')
+        {
+            ans += i;
+            ans += '+';
+        }
+    }
+ 
+    ans.pop_back();
+    cout << ans;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
