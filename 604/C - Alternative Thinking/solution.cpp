cd879080f2925d9aefdf9016ee02ce156fd69731
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 14:06:32 +0530

    Add Alternative Thinking [C] from Codeforces

---
+++ 604/C - Alternative Thinking/solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    string str;
+    cin >> str;
+ 
+    int change = 1;
+ 
+    for (int i = 1; i < n; i++)
+    {
+        if (str[i] != str[i - 1])
+            change++;
+    }
+ 
+    cout << min(n, change + 2);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
