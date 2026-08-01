91f1c43d86fb7bbef21bcd703abdca2dfff4a7f1
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:52:44 +0530

    Add Theatre Square [A] from Codeforces

---
+++ 1/A - Theatre Square/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, m, a;
+    cin >> n >> m >> a;
+ 
+    cout << ((n + a - 1) / a) * ((m + a - 1) / a);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
