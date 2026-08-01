5c6234dae6dae51737ebd6d2133b9f382617f2cf
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:58:58 +0530

    Add Two Buttons [B] from Codeforces

---
+++ 520/B - Two Buttons/solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+ 
+    if (n >= m)
+        cout << n - m;
+    else
+    {
+        int ans = 0;
+        while (m > n)
+        {
+            if (m % 2 == 0)
+                m /= 2;
+            else
+                m++;
+            ans++;
+        }
+ 
+        cout << ans + (n - m);
+    }
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
