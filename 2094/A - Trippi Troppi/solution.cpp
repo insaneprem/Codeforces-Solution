941ff9ab98cdc2dd7ec4e8aad25bf103510f0467
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:37:11 +0530

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    string a, b, c;
+    cin >> a >> b >> c;
+ 
+    cout << a[0] << b[0] << c[0];
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
