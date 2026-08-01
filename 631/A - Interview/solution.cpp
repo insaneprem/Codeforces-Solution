cadb89d4a7da58cbb2e71669e7d084f2c5428144
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 14:09:19 +0530

    Add Interview [A] from Codeforces

---
+++ 631/A - Interview/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    vector<int> arr(n), brr(n);
+    int a = 0, b = 0;
+    for (auto &i : arr)
+        cin >> i, a |= i;
+    for (auto &i : brr)
+        cin >> i, b |= i;
+ 
+    cout << a + b;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
