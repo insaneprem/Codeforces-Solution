9b59f25a8ea2915b7aba3197b17be8c36f7410d2
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:55:38 +0530

    Add Soldier and Bananas [A] from Codeforces

---
+++ 546/A - Soldier and Bananas/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int k, n, w;
+    cin >> k >> n >> w;
+ 
+    cout << max(0LL, ((w * (w + 1) / 2) * k) - n);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
