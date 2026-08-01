e78941be8712c54fc7554ede9d3f9f79ce8d02d5
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:32:12 +0530

    Add Grouping Increases [C] from Codeforces

---
+++ 1919/C - Grouping Increases/solution.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    vector<int> arr(n);
+    for (auto &i : arr)
+        cin >> i;
+ 
+    int a = 1e8, b = 1e8, ans = 0;
+ 
+    for (auto i : arr)
+    {
+        if (a > b)
+            swap(a, b);
+ 
+        if (i <= a)
+            a = i;
+        else if (i <= b)
+            b = i;
+        else
+            a = i, ans++;
+    }
+ 
+    cout << ans;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
