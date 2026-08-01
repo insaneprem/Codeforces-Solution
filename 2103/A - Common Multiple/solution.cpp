a8cffeae9230ec4e0906634e2185a47919d50e09
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:39:02 +0530

    Add Common Multiple [A] from Codeforces

---
+++ 2103/A - Common Multiple/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        mpp[val]++;
+    }
+ 
+    cout << mpp.size();
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
