e9d1320f8419183f88b783dddab7b359d101d8b8
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:54:20 +0530

    Add Polo the Penguin and Segments  [A] from Codeforces

---
+++ 289/A - Polo the Penguin and Segments /solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+ 
+    int val = 0;
+ 
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+ 
+        val += b - a + 1;
+    }
+ 
+    cout << ((((val + k - 1) / k) * k) - val);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
