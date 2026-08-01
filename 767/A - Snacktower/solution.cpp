29ef84c74a9c1a27283da1cece14e683b34d2cd5
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:51:13 +0530

    Add Snacktower [A] from Codeforces

---
+++ 767/A - Snacktower/solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    int size = n;
+    set<int> st;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        st.insert(val);
+ 
+        while (!st.empty() && st.count(size))
+        {
+            cout << size << " ";
+            st.erase(size);
+            size--;
+        }
+        cout << '
+';
+    }
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
