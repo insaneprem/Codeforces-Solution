649d9d1330775cf05b7e68a3b3fb9aaa46eaafba
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:39:27 +0530

    Add Sasha and the Apartment Purchase [B] from Codeforces

---
+++ 2098/B - Sasha and the Apartment Purchase/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+ 
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+ 
+    vector<int> arr(n);
+    for (auto &i : arr) cin >> i;
+    
+    sort(arr.begin(), arr.end());
+ 
+    cout << arr[(n + k) / 2] - arr[(n - k - 1) / 2] + 1;
+}
+ 
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
