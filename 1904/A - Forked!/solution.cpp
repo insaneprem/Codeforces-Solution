b2af4b0a2264eb78340caab101472bf87000449c
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 14:22:02 +0530

    Add Forked! [A] from Codeforces

---
+++ 1904/A - Forked!/solution.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define ld long double
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "
+"
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " -> "; _print(x); cerr << '
+';
+#else
+#define debug(x)
+#endif
+ 
+void _print(int t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(const string &t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(bool t) { cerr << (t ? "true" : "false"); }
+ 
+template <class T, class V> void _print(const pair<T, V> &p);
+template <class T> void _print(const vector<T> &v);
+template <class T> void _print(const vector<vector<T>> &v);
+template <class T> void _print(const set<T> &v);
+template <class T> void _print(const multiset<T> &v);
+template <class T, class V> void _print(const map<T, V> &v);
+template <typename... Args> void _print(const tuple<Args...> &t);
+ 
+ 
+template <class T, class V> void _print(const pair<T, V> &p) {cerr << "{"; _print(p.first); cerr << ",";
+_print(p.second); cerr << "}";}
+template <class T> void _print(const vector<T> &v) {cerr << "[ "; for (const auto &i : v) {_print(i); cerr << " ";}
+cerr << "]";}
+template <class T> void _print(const vector<vector<T>> &v) {cerr << "[
+"; for (const auto &row : v) {cerr << "  [ ";
+for (const auto &x : row) {_print(x); cerr << " ";} cerr << "]
+";} cerr << "]";}
+template <class T> void _print(const set<T> &v) {cerr << "[ "; for (const auto &i : v) { _print(i); cerr << " ";}
+cerr << "]";}
+template <class T> void _print(const multiset<T> &v) {cerr << "[ "; for (const auto &i : v) { _print(i); cerr << " ";}
+cerr << "]";}
+template <class T, class V> void _print(const map<T, V> &v) {cerr << "[ "; for (const auto &i : v) { _print(i);
+cerr << " "; } cerr << "]";}
+template <size_t idx = 0, typename... T> typename enable_if<idx == sizeof...(T)>::type _print_tuple(const tuple<T...> &t) {}
+template <size_t idx = 0, typename... T>
+typename enable_if<idx < sizeof...(T)>::type
+_print_tuple(const tuple<T...> &t) {
+    if(idx > 0) cerr << ",";
+    _print(get<idx>(t));
+    _print_tuple<idx+1>(t);
+}
+template <typename... Args> void _print(const tuple<Args...> &t) {cerr << "{"; _print_tuple(t); cerr << "}";}
+ 
+// ----------------------------------------------------------------------------------------------------------------->
+void solve()
+{
+    int a, b, kx, ky, qx, qy;
+    cin >> a >> b >> kx >> ky >> qx >> qy;
+ 
+    int dx[] = {a, a, -a, -a, b, b, -b, -b};
+    int dy[] = {b, -b, b, -b, a, -a, a, -a};
+ 
+    int ans = 0;
+    set<pair<int,int>> st;
+    for (int k = 0; k < 8; k++)
+    {
+        int ni = kx + dx[k];
+        int nj = ky + dy[k];
+ 
+        for (int j = 0; j < 8; j++)
+        {
+            int nii = ni + dx[j];
+            int njj = nj + dy[j];
+            
+            if((nii == qx) && (njj == qy)){
+                pair<int,int> it = {ni , nj};
+                st.insert(it);
+            }
+        }
+    }
+ 
+    cout << st.size();
+}
+ 
+signed main() {
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+    
+    fastio();
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+        cout << "
+";
+    }
+    return 0;
+}
