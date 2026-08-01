d73fbf8bd4ce7328bd90df13ecd721ad54063201
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 14:06:36 +0530

    Add Playing Cubes [B] from Codeforces

---
+++ 257/B - Playing Cubes/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+ 
+    if (a < b)
+        swap(a, b);
+ 
+    cout << a - 1 << " " << b;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
