3d9c1dee931a9ab51d83e62bed90b27398f307eb
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:51:33 +0530

    Add Even Odds [A] from Codeforces

---
+++ 318/A - Even Odds/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+ 
+    int totalodd = (n + 1) / 2;
+ 
+    if (k <= totalodd)
+        cout << 2 * (k - 1) + 1;
+    else
+        cout << 2 * (k - totalodd);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
