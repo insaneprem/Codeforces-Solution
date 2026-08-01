19380abd035e5af88a39e9d8a8342d064fc79d1c
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:50:56 +0530

    Add New Password [A] from Codeforces

---
+++ 770/A - New Password/solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+ 
+    int uniquechar = n;
+ 
+    string ans = "";
+    for (int i = 0; i < k; i++)
+        ans += ('a' + i);
+ 
+    while (ans.size() < n)
+        ans += ans;
+ 
+    cout << ans.substr(0, n);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
