cdf6a72732e1f94c2890626282229b66e4daeb55
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:30:12 +0530

    Add Make Almost Equal With Mod [B] from Codeforces

---
+++ 1909/B - Make Almost Equal With Mod/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    vector<int> arr(n);
+    for (auto &i : arr)
+        cin >> i;
+ 
+    for (int i = 2;; i <<= 1)
+    {
+        set<int> st;
+ 
+        for (auto num : arr)
+            st.insert(num % i);
+ 
+        if (st.size() == 2)
+        {
+            cout << i;
+            break;
+        }
+    }
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
