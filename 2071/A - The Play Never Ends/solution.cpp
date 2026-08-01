cf29ca2677bc90484e56334a45ea9534f982ff29
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:28:22 +0530

    Add The Play Never Ends [A] from Codeforces

---
+++ 2071/A - The Play Never Ends/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define ld long double
+void solve(){
+    int k;
+    cin>>k;
+ 
+    if(k == 1) cout<<"YES";
+    else if(k == 2) cout<<"No";
+    else if(k%3 == 1) cout<<"YES";
+    else cout<<"No";
+}
+signed main(){
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+        cout<<"
+";
+    }
+}
