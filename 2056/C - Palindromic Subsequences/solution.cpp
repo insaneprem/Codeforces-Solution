a35a871763339c8c61b4c4b96a776738c4e7a62f
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:30:04 +0530

    Add Palindromic Subsequences [C] from Codeforces

---
+++ 2056/C - Palindromic Subsequences/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    cout << "1 1 ";
+    for (int i = 2; i <= n - 2; i++)  cout << i << " ";
+ 
+    cout << 1;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
