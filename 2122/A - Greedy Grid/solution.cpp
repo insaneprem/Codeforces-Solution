ffd116432ab54655b5ab63c846b2d9deff9dbde9
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:46:24 +0530

    Add Greedy Grid [A] from Codeforces

---
+++ 2122/A - Greedy Grid/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    if (min(n, m) >= 2 && max(n, m) >= 3)
+        cout << "YES";
+    else
+        cout << "NO";
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
