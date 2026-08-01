4d2074bffef7ef3047861e6e86a6dde84652854f
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:56:54 +0530

    Add Caisa and Pylons [B] from Codeforces

---
+++ 463/B - Caisa and Pylons/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    vector<int> arr(n + 1, 0);
+ 
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
+ 
+    cout<<*max_element(begin(arr),end(arr));
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
