1de48265b3c5d95f28e25881a21d1f7ef61e6ede
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:47:50 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+ 
+    for (int i = 1;; i++)
+    {
+        a *= 3;
+        b *= 2;
+ 
+        if (a > b)
+        {
+            cout << i;
+            break;
+        }
+    }
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
