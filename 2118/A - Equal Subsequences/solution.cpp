95d9e7d8dce415f692e0c8cc97cd861712fa600d
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:42:45 +0530

    Add Equal Subsequences [A] from Codeforces

---
+++ 2118/A - Equal Subsequences/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    cout << string(k, '1') << string(n - k, '0');
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
