027664cc348049ecba8d30c249d574bd9350e1f1
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 14:12:38 +0530

    Add Increase and Decrease [B] from Codeforces

---
+++ 246/B - Increase and Decrease/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    int sum = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+ 
+        sum += val;
+    }
+ 
+    cout << ((sum % n == 0) ? n : n - 1);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
