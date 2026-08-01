ad63b8b6029d9613b3c4eef2c8020c0df45413ef
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:23:33 +0530

    Add MEX Table [A] from Codeforces

---
+++ 2057/A - MEX Table/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define ld long double
+void solve(){
+  int n,m;
+  cin>>n>>m;
+ 
+  cout<<max(n,m)+1;
+} 
+signed main(){
+    // Humare saath Shree Raghunath to kisi baat ki chita nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+        cout<<"
+";
+    }
+}
