14099f96e728de6e2b3378adea5d75d914c1c135
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:28:49 +0530

    Add Remove Two Letters [D] from Codeforces

---
+++ 1800/D - Remove Two Letters/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    string str;
+    cin >> str;
+ 
+    int ans = n - 1;
+    for (int i = 1; i + 1 < n; i++)
+        if (str[i - 1] == str[i + 1])
+            ans--;
+ 
+    cout << ans;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
