a21d2b97d90a063adf81b793dbeb02ff763dee24
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 14:21:15 +0530

    Add Make it Divisible by 25 [B] from Codeforces

---
+++ 1593/B - Make it Divisible by 25/solution.cpp
@@ -0,0 +1,182 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define ld long double
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "
+"
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " -> "; _print(x); cerr << '
+';
+#else
+#define debug(x)
+#endif
+ 
+void _print(int t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(const string &t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(bool t) { cerr << (t ? "true" : "false"); }
+ 
+template <class T, class V> void _print(const pair<T, V> &p);
+template <class T> void _print(const vector<T> &v);
+template <class T> void _print(const vector<vector<T>> &v);
+template <class T> void _print(const set<T> &v);
+template <class T> void _print(const multiset<T> &v);
+template <class T, class V> void _print(const map<T, V> &v);
+template <typename... Args> void _print(const tuple<Args...> &t);
+ 
+ 
+template <class T, class V> void _print(const pair<T, V> &p) {cerr << "{"; _print(p.first); cerr << ",";
+_print(p.second); cerr << "}";}
+template <class T> void _print(const vector<T> &v) {cerr << "[ "; for (const auto &i : v) {_print(i); cerr << " ";}
+cerr << "]";}
+template <class T> void _print(const vector<vector<T>> &v) {cerr << "[
+"; for (const auto &row : v) {cerr << "  [ ";
+for (const auto &x : row) {_print(x); cerr << " ";} cerr << "]
+";} cerr << "]";}
+template <class T> void _print(const set<T> &v) {cerr << "[ "; for (const auto &i : v) { _print(i); cerr << " ";}
+cerr << "]";}
+template <class T> void _print(const multiset<T> &v) {cerr << "[ "; for (const auto &i : v) { _print(i); cerr << " ";}
+cerr << "]";}
+template <class T, class V> void _print(const map<T, V> &v) {cerr << "[ "; for (const auto &i : v) { _print(i);
+cerr << " "; } cerr << "]";}
+template <size_t idx = 0, typename... T> typename enable_if<idx == sizeof...(T)>::type _print_tuple(const tuple<T...> &t) {}
+template <size_t idx = 0, typename... T>
+typename enable_if<idx < sizeof...(T)>::type
+_print_tuple(const tuple<T...> &t) {
+    if(idx > 0) cerr << ",";
+    _print(get<idx>(t));
+    _print_tuple<idx+1>(t);
+}
+template <typename... Args> void _print(const tuple<Args...> &t) {cerr << "{"; _print_tuple(t); cerr << "}";}
+ 
+// ----------------------------------------------------------------------------------------------------------------->
+void solve() {
+    string str;
+    cin>>str;
+    
+    int n = str.size();
+    auto ans25 = [=]()
+    {
+        int idx5 = n;
+        int pcnt = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (str[i] == '5')
+            {
+                idx5 = i;
+                break;
+            }
+            if(str[i] > '0') pcnt++;
+        }
+ 
+        if (idx5 == n)
+            return (long long)1e6;
+ 
+        for (int i = idx5 - 1; i >= 0; i--)
+        {
+            
+            if(str[i] == '2'){
+                return pcnt;
+            }
+            pcnt++;
+        }
+ 
+        return (long long)1e6;
+    };
+    auto ans75 = [=]()
+    {
+        int idx5 = n;
+        int pcnt = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (str[i] == '5')
+            {
+                idx5 = i;
+                break;
+            }
+            if(str[i] > '0') pcnt++;
+        }
+ 
+        if (idx5 == n)
+            return (long long)1e6;
+ 
+        for (int i = idx5 - 1; i >= 0; i--)
+        {
+            
+            if(str[i] == '7'){
+                return pcnt;
+            }
+            pcnt++;
+        }
+ 
+        return (long long)1e6;
+    };
+ 
+    auto ans50 = [=]()
+    {
+        int zcnt = 0, pcnt = 0;
+ 
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (str[i] == '5' && zcnt > 0)
+            {
+                return pcnt;
+            }
+ 
+            if (str[i] > '0')
+                pcnt++;
+            else
+                zcnt++;
+        }
+        return (long long)1e6;
+    };
+    auto ans00 = [=]()
+    {
+        int zcnt = 0, pcnt = 0;
+ 
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (str[i] == '0' && zcnt > 0)
+            {
+                return pcnt;
+            }
+ 
+            if (str[i] > '0')
+                pcnt++;
+            else
+                zcnt++;
+        }
+        return (long long)1e6;
+    };
+    
+    int a = ans00();
+    int b = ans25();
+    int c = ans50();
+    int d = ans75();
+ 
+    debug(a);
+    debug(b);
+    debug(c);
+    debug(d);
+    int ans = min({ans25(), ans75(), ans50(), ans00()});
+ 
+    cout<<ans;
+}
+ 
+signed main() {
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+    
+    fastio();
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+        cout << "
+";
+    }
+    return 0;
+}
