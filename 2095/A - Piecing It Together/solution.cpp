04306703d5911f22ba788a1ad01ab39cfcb9182d
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:34:08 +0530

    Add Piecing It Together [A] from Codeforces

---
+++ 2095/A - Piecing It Together/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define ld long double
+void solve(){
+   cout<<"puzzling";
+}
+signed main(){
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t=1;
+    // cin>>t;
+    while(t--) {
+        solve();
+        cout<<"
+";
+    }
+}
