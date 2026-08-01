8f3a4c6bf2d8c0f66587767128dcf06ccf4f68a6
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:37:38 +0530

    Add Chloe and the sequence  [B] from Codeforces

---
+++ 743/B - Chloe and the sequence /solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+ 
+    int cnt = 0;
+ 
+    while (k % 2 != 1)
+        k /= 2, cnt++;
+ 
+    cout << cnt + 1;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
