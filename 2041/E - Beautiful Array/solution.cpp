ff4971a8c657b7df0112b34a723528e2780f13cd
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:29:37 +0530

    Add Beautiful Array [E] from Codeforces

---
+++ 2041/E - Beautiful Array/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int mean, median;
+    cin >> mean >> median;
+    cout << 3 << "
+";
+    cout << median << " " << median << " " << (3 * mean) - (2 * median);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
