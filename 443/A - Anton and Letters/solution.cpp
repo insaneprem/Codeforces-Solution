fd4b9f18cd687f1e1c2e6deb96c35366ea76768d
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:50:02 +0530

    Add Anton and Letters [A] from Codeforces

---
+++ 443/A - Anton and Letters/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    string str;
+    getline(cin, str);
+ 
+    map<int, int> mp;
+    for (auto i : str)
+    {
+        if (isalpha(i))
+            mp[i]++;
+    }
+ 
+    cout << mp.size();
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
