d62d191031cbfbb5159d174792ebd19ee418945a
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:33:13 +0530

    Add Combination Lock [C] from Codeforces

---
+++ 2091/C - Combination Lock/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    if (n % 2 == 1)
+    {
+        for (int i = 1; i <= n; i += 2)
+            cout << i << " ";
+        for (int i = 2; i <= n; i += 2)
+            cout << i << " ";
+    }
+    else
+        cout << -1;
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
