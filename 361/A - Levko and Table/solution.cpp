a569b002b1dc0ddc6883e7a60418f266007469eb
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:55:47 +0530

    Add Levko and Table [A] from Codeforces

---
+++ 361/A - Levko and Table/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                cout << k << " ";
+            else
+                cout << 0 << " ";
+        }
+ 
+        cout << "
+";
+    }
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
