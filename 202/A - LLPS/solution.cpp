a7dd3b20a78dc2dcacc56db7396cd1b89ca5a969
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:38:01 +0530

    Add LLPS [A] from Codeforces

---
+++ 202/A - LLPS/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+void solve()
+{
+    string str;
+    cin >> str;
+ 
+    char ch = *max_element(begin(str), end(str));
+ 
+    int freq = count(begin(str), end(str), ch);
+ 
+    cout << string(freq, ch);
+}
+signed main()
+{
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        cout << "
+";
+    }
+}
