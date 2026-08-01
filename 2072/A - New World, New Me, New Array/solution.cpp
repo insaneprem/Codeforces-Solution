68c10c93e9339883989e53fab240dd8d5c48fd72
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2026-08-01 13:27:09 +0530

    Add New World, New Me, New Array [A] from Codeforces

---
+++ 2072/A - New World, New Me, New Array/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define ld long double
+void solve(){
+   int n,k,p;
+   cin>>n>>k>>p;
+   
+   k=abs(k);
+   int val=(k+p-1)/p;
+ 
+   if(val>n) cout<<-1;
+   else cout<<val;
+}
+signed main(){
+    // Humare saath Shree Raghunath to kisi baat ki chinta nahi
+ 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+        cout<<"
+";
+    }
+}
